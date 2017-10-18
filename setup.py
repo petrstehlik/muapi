dbdf256715448d5054c4a90c301a36d426282276
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-10-18 18:07:11 +0200

    Fix my name and publish to PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
 setup(
   name = 'muapi',
   packages = ['muapi', 'muapi.modules'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'Modular user-oriented REST API',
-  author = 'Petr Stehlik',
+  author = u'Petr Stehlík',
   author_email = 'pe.stehlik@gmail.com',
   url = 'https://github.com/petrstehlik/muapi',
   download_url = 'https://github.com/petrstehlik/muapi/archive/0.1.tar.gz',
